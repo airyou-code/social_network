8ccd5ae8dfe062a311a2bc786a556ef75a64ef72
Artem Kirillov <holod7272@outlook.com>
2022-05-16 03:35:47 +0200

    dd

--- ff/main/views.py
+++ ff/main/views.py
@@ -6,27 +6,31 @@ import random
 import string
 
 def index(request):
-    
+    return HttpResponseRedirect(f"/login")
     return render(request, 'main/index.html')
 
 
 def ls(request, pk):
     try:
         user = User.objects.get(id=pk)
-        if 'token' in request.COOKIES:
-            token = request.COOKIES['token']
-            if token == user.token:
-                return render(request, "main/index.html", {"user": user})
+        if 'user_id' in request.COOKIES:
+            if request.COOKIES['user_id'] == str(pk):
+                if 'token' in request.COOKIES:
+                    token = request.COOKIES['token']
+                    if token == user.token:
+                        return render(request, "main/index.html", {"user": user, "salf_page": True})
+                    else:
+                        response = HttpResponseRedirect(f"/login")
+                        response.delete_cookie('token')
+                        response.delete_cookie('user_id')
+                        return response
+                else:
+                    response = HttpResponseRedirect(f"/login")
+                    response.delete_cookie('token')
+                    response.delete_cookie('user_id')
+                    return response
             else:
-                response = HttpResponseRedirect(f"/login")
-                response.delete_cookie('token')
-                response.delete_cookie('user_id')
-                return response
-        else:
-            response = HttpResponseRedirect(f"/login")
-            response.delete_cookie('token')
-            response.delete_cookie('user_id')
-            return response
+                return render(request, "main/index.html", {"user": user, "salf_page": False})
 
     except User.DoesNotExist:
         return HttpResponseNotFound("<h2>Test not found</h2>")
@@ -60,26 +64,29 @@ def logout(request):
     pass
 
 def edit(request):
-    try:
-        user = User.objects.get(id=request.COOKIES['user_id'])
-        if request.method == "POST":
-            if user.token == request.COOKIES['token']:
-                token = random_token(20)
-                user.token = token
-                user.login = request.POST.get("login")
-                user.description = request.POST.get("description")
-                user.save()
-                response = HttpResponseRedirect(f"/{user.id}")
-                response.set_cookie('token', token)
-                response.set_cookie('user_id', user.id)
-                return response
-                pass
-        else:
-            return render(request, 'main/edit.html', {"user": user})
+    if 'token' in request.COOKIES and 'user_id' in request.COOKIES:
+        try:
+            user = User.objects.get(id=request.COOKIES['user_id'])
+            if request.method == "POST":
+                if user.token == request.COOKIES['token']:
+                    token = random_token(20)
+                    user.token = token
+                    user.login = request.POST.get("login")
+                    user.description = request.POST.get("description")
+                    user.save()
+                    response = HttpResponseRedirect(f"/{user.id}")
+                    response.set_cookie('token', token)
+                    response.set_cookie('user_id', user.id)
+                    return response
+                    pass
+            else:
+                return render(request, 'main/edit.html', {"user": user})
 
-    except User.DoesNotExist:
-        return HttpResponseNotFound("<h2>user not found</h2>")
-    pass
+        except User.DoesNotExist:
+            return HttpResponseNotFound("<h2>user not found</h2>")
+        pass
+    else: 
+        return HttpResponseRedirect(f"/login")
     pass
 
 def random_token(length):
